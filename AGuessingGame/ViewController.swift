a988135a2ff3cc0d703af8f6cd4e54d3acd6032d
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2019-09-30 16:31:23 -0400

    added opening message

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    
+    
+    //MARK: Initializers
+    
+    
+    //MARK: Methods (functions) - behaviours
+    
+    //runs as soon as the view becomes visible to the user
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //make an object named 'synthesizer', which is an instance of the
+        //class 'AVSpeechSynthesizer'
+        let synthesizer = AVSpeechSynthesizer()
+        
+       //make a string that contains what we want our computer to say
+        let message = "I'm thinking of a number between 1 and 100. Guess what it is"
+        
+        //Make an object named 'utterance', which is an instance of the class
+        //'AVSpeechUtterance'
+        let utterance = AVSpeechUtterance(string: message)
+        
+        //speak the message
+        synthesizer.speak(utterance)
     }
 
 
